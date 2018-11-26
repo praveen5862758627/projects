2c3e6f043ef0d2304c92e9c1d6e952185c79ca15
praveen5862758627 <45360533+praveen5862758627@users.noreply.github.com>
2018-11-26 19:02:42 +0530

    Add files via upload

---
+++ dbver.php
@@ -0,0 +1,10 @@
+<?php
+echo phpversion();
+
+/*$link = mysql_connect('recruitment.rewardingsurveys.cn', 'bapraveen', 'dz!--&rg-l!M');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);*/
+?>
